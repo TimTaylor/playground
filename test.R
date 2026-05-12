ea21dcb0f98e2cadd76e70f6cf787ef59f4e7550
Tim Taylor <tim.taylor@hiddenelephants.co.uk>
2026-05-12 21:14:14 +0100

    meh

---
+++ test.R
@@ -0,0 +1,20 @@
+f <- tempfile()
+if (!dir.create(f))
+    stop("Unable to create temporary directory for integration test")
+files <- list.files("tests", full.names = TRUE)
+if (!all(file.copy(files, f)))
+    stop("Copying test files failed.")
+expected <- readRDS("tests/out.rds")
+
+od <- setwd(f)
+tryCatch(
+    expr = {
+        source("out.R")
+        if (!isTRUE(all.equal(out, 2))) {
+            stop("Integration test failed.")
+        }
+    },
+    finally = setwd(od)
+)
+
+
